16c6b400bc664951f4a72fb394859b424ea18ae0
sohelrana2250 <amsr215019@gmail.com>
2024-06-05 09:13:07 +0600

    final

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,11 +1,11 @@
 import { Link, Outlet } from "react-router-dom";
-import useAdmin from "../hooks/useAdmin";
+//import useAdmin from "../hooks/useAdmin";
 import useAuth from "../hooks/useAuth";
 import Navbar from "../components/Navbar";
 
 const DashboardLayout = () => {
   const { user } = useAuth();
-  const [isAdmin] = useAdmin(user?.email);
+  //const [isAdmin] = useAdmin(user?.email);
 
   return (
     <>
@@ -23,22 +23,21 @@ const DashboardLayout = () => {
           ></label>
           <ul className="menu p-4 w-72 min-h-full bg-base-200 text-base-content">
             <ul>
-              {isAdmin && (
-                <>
-                  <li className="m-1">
-                    <Link to={"/dashboard"}>Dashboard</Link>
-                  </li>
-                  <li className="m-1">
-                    <Link to={"/dashboard/all-products"}>All Products</Link>
-                  </li>
-                  <li className="m-1">
-                    <Link to={"/dashboard/add-products"}>Add Product</Link>
-                  </li>
-                  {/* <li className="m-1">
+              <>
+                <li className="m-1">
+                  <Link to={"/dashboard"}>Dashboard</Link>
+                </li>
+                <li className="m-1">
+                  <Link to={"/dashboard/all-products"}>All Products</Link>
+                </li>
+                <li className="m-1">
+                  <Link to={"/dashboard/add-products"}>Add Product</Link>
+                </li>
+                {/* <li className="m-1">
                     <Link to={"/dashboard/all_user"}>All User</Link>
                   </li> */}
-                </>
-              )}
+              </>
+
               <li className="m-1">
                 <Link to={"/"}>Home</Link>
               </li>
